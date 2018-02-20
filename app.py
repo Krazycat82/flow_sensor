26ad226fe59e2a0fbb34f79ae3cf12d72b9d823e
mbuerge <marita.buerge@target.com>
2018-02-19 20:47:11 -0600

    added mongo support

--- app.py
+++ app.py
@@ -8,42 +8,48 @@ curl http://localhost:5000/flow_sensor/api/v1.0/amounts
 '''
 from flask import Flask,jsonify, request
 from json2html import *
+from pymongo import MongoClient
+
+# Connect to the MongoDB, change the connection string per your MongoDB environment
+client = MongoClient('mongodb://thedots:vAwru&ax8zaB@ds241668.mlab.com:41668/dripdrop')
+db=client.dripdrop
 
 app = Flask(__name__) # Intializes Library
 
 #TODO remove fake data Empty array
 
-amounts_array = []
-    
+# amounts_array = []
+
 # getting data
-@app.route('/flow_sensor/api/v1.0/amounts', methods=['GET']) 
+@app.route('/flow_sensor/api/v1.0/amounts', methods=['GET'])
 def get_amounts():
-    return jsonify({'amounts': amounts_array})
+    amounts = []
+    for doc in db.amounts.find({}, {'_id': False}):
+        print(doc)
+        amounts.append(doc)
+    print(amounts)
+    return jsonify({'amounts': amounts})
 
 # saving data
 @app.route('/flow_sensor/api/v1.0/amounts', methods=['POST'])
 def post_amounts():
     if not request.json or not 'amount' in request.json:
         abort(400)
-    NumberOfAmounts = len(amounts_array)
-    print(NumberOfAmounts)
-    ID = NumberOfAmounts + 1 
-    
     # creating a json object
     amount = {
-        'id': ID,  
+        # 'id': ,
         'timestamp': request.json['timestamp'],
         'amount': request.json['amount'],
         'duration_in_seconds': request.json['duration_in_seconds']
     }
-    amounts_array.append(amount)   # adds amount to amounts array
-    return jsonify({'amount': amount}), 201
+    # amounts_array.append(amount)   # adds amount to amounts array
+    result=db.amounts.insert_one(amount)
+    print(amount)
+    return str(amount), 201
 
 @app.route('/')
 def index():
     infoFromJson = get_amounts()
-    
-#    print json2html.convert(json = infoFromJson)
     return "POTATO POTATO"
 
 if __name__ == '__main__':
