51e22877f0a439e58d9d1d122628b5de724b323f
mbuerge <marita.buerge@target.com>
2018-04-16 09:15:46 -0500

    fixed timestamp

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def post_amounts():
     # creating a json object
     amount = request.json['amount']
     duration_in_seconds = request.json['duration_in_seconds']
-    timestamp = datetime.strptime(request.json['timestamp'], '%D %T')
+    timestamp = datetime.datetime.strptime(request.json['timestamp'], '%D %T')
     # timestamp = request.json['timestamp']
     # timestamp = datetime.datetime.now()
     water_usage = process(amount, duration_in_seconds, timestamp, "Coco")
